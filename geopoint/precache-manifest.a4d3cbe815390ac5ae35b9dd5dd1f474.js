8ea295659e530ed9659fb0934ae030f139bd9951
Margodt Miguel <miguel.margodt@student.howest.be>
2018-10-28 16:05:19 +0100

    map page + random map picker

---
+++ geopoint/precache-manifest.a4d3cbe815390ac5ae35b9dd5dd1f474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/geopoint/robots.txt"
+  },
+  {
+    "revision": "a25a81f58e71fb67f3ca",
+    "url": "/geopoint/js/chunk-vendors.378814a5.js"
+  },
+  {
+    "revision": "39ca41aee13b18efccde",
+    "url": "/geopoint/js/app.44b01aec.js"
+  },
+  {
+    "revision": "88a4276091130ee818f1d5efc7d25c16",
+    "url": "/geopoint/index.html"
+  },
+  {
+    "revision": "1651425f72267c03939b62e4469f7689",
+    "url": "/geopoint/img/europe.1651425f.svg"
+  },
+  {
+    "revision": "39ca41aee13b18efccde",
+    "url": "/geopoint/css/app.78746f62.css"
+  }
+];
--- geopoint/service-worker.js
+++ geopoint/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/geopoint/precache-manifest.f3acc1335bf1e30bba3df3c2972bae29.js"
+  "/geopoint/precache-manifest.a4d3cbe815390ac5ae35b9dd5dd1f474.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "geopoint"});
