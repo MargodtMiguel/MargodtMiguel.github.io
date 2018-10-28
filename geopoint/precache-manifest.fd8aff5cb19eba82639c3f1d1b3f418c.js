1370a1c4428b7feac58675f12394e0e74a73e73b
Margodt Miguel <miguel.margodt@student.howest.be>
2018-10-28 11:39:34 +0100

    geopoint mobile test

---
+++ geopoint/precache-manifest.fd8aff5cb19eba82639c3f1d1b3f418c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/geopoint/robots.txt"
+  },
+  {
+    "revision": "a25a81f58e71fb67f3ca",
+    "url": "/geopoint/js/chunk-vendors.378814a5.js"
+  },
+  {
+    "revision": "ddd901ab5fbcd06f2e7d",
+    "url": "/geopoint/js/app.869dd56a.js"
+  },
+  {
+    "revision": "3103995445a1c4b2db61f95b242d8e61",
+    "url": "/geopoint/index.html"
+  },
+  {
+    "revision": "ddd901ab5fbcd06f2e7d",
+    "url": "/geopoint/css/app.23cf8d33.css"
+  }
+];
