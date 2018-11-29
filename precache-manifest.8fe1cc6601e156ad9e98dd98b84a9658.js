725bbc2a32f46292c0d3ee0f2cd37b1541b1c7d9
Miguel Margodt <miguel.margodt@student.howest.be>
2018-11-29 14:25:40 +0100

    gp

---
+++ precache-manifest.8fe1cc6601e156ad9e98dd98b84a9658.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "acaf0e8a2c3b2ce10d0f56761eebfa96",
+    "url": "/img/trash.acaf0e8a.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0bf728299dea44756b39",
+    "url": "/js/chunk-vendors.f345aec9.js"
+  },
+  {
+    "revision": "df6608ce3fb9489d84f1",
+    "url": "/js/app.ae6f881f.js"
+  },
+  {
+    "revision": "67500565f5e9836fd6c547e822bf4511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae544cd7f5671be3897e39c079bc54f8",
+    "url": "/img/translation.ae544cd7.png"
+  },
+  {
+    "revision": "7d4514edbfe3b3fcceb4d6467dc9c11c",
+    "url": "/img/europe.7d4514ed.svg"
+  },
+  {
+    "revision": "6ba6181627552dc62359915615ee15cb",
+    "url": "/img/cross.6ba61816.png"
+  },
+  {
+    "revision": "0a6191067c874bbcac32e68c9573f954",
+    "url": "/img/checkmark.0a619106.png"
+  },
+  {
+    "revision": "4bfa11f2bfb64fc9a87472104360de0d",
+    "url": "/img/africa.4bfa11f2.svg"
+  },
+  {
+    "revision": "7e04b8738bf4b92f959d5885cf1f6039",
+    "url": "/img/addFriend.7e04b873.png"
+  },
+  {
+    "revision": "df6608ce3fb9489d84f1",
+    "url": "/css/app.5ca5b5d6.css"
+  }
+];
