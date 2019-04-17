a0f9dde1253ab730d31e4b50bae2af06e05bd01b
margodtmiguel <miguel.margodt@student.howest.be>
2019-04-17 18:00:47 +0200

    counter

---
+++ counter/precache-manifest.7e42a5eebde9c5f6352ec07517fafe62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4e6d13d28f11c0635738",
+    "url": "/js/chunk-vendors.430168e3.js"
+  },
+  {
+    "revision": "5cb1e4d3d0a7c2cb3c16",
+    "url": "/js/app.8e9a42fb.js"
+  },
+  {
+    "revision": "bef10c5b5fb13c22fd9ba7305521d54a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cb1e4d3d0a7c2cb3c16",
+    "url": "/css/app.2bef7db7.css"
+  }
+];
